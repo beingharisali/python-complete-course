2411924e7abd7f1104de23815ce16e4af56a0a49
beingharisali <theharis99@gmail.com>
2025-10-05 19:47:23 +0500

    initial commit

---
+++ intro.py
@@ -0,0 +1,26 @@
+# myname = "Haris" # string
+# age = "37" # string
+# print("The username is " + myname + " and his age is " + age)
+# cannot concat string with integer
+# num1 = 5 # integer
+# num2 = 10.0 # float
+# print(num1+num2)
+# isThisInfoTrue # camelCase
+# IsThisInfoTrue # PascalCase
+# isthisinfotrue # kababcase
+# is_this_info_true # snake_case
+# isThisInfoTrue = True
+# print(isThisInfoTrue)
+
+# myName = "Admin"
+# myAge = 37
+# print("The user name is " +myName + "and his age is " + str(myAge))
+
+# Calculator
+num1 = 5
+num2 = 5
+# Arithmetic Operators
+print("The sum between num1 and num2 is ", num1+num2)
+print("The subtraction between num1 and num2 is ", num1-num2)
+print("The multiplication between num1 and num2 is ", num1*num2)
+print("The division between num1 and num2 is ", num1/num2)
