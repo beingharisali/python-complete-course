bb70993bee3e1ba11568de7d81b4aa901b39d728
beingharisali <theharis99@gmail.com>
2025-10-11 18:20:56 +0500

    added datatypes file

---
+++ datatypes.py
@@ -0,0 +1,9 @@
+character_name = "Haris" # string
+character_age = "37" # string
+character_age_num = 37 # integer
+is_this_info_true = False
+print("Hey the name is " + character_name)
+print(character_name + " is going to be " + str(character_age_num) + " this year")
+print(character_name + " likes to gain more experience and knowledge")
+print("But " +  character_name + " doestn't like to be " + str(character_age_num) + " years old")
+print("This whole information is ", is_this_info_true)
