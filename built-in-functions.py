df8e3784eedc81d393a9c3288278945b743181ee
beingharisali <theharis99@gmail.com>
2025-10-11 18:40:50 +0500

    added built in functions file

---
+++ built-in-functions.py
@@ -0,0 +1,13 @@
+# phrase = "Hey This is Just a String"
+# print(phrase.upper()) # to convert string in uppercase
+# print(phrase.lower()) # to convert string in lowercase
+# print(phrase.lower().islower()) # to check if string is in lowercase or uppercase
+# print(len(phrase)) # to check length of a string
+# print(phrase[0]) # to get character of specific index
+# print(phrase.index('Just')) # to get index of a specific character
+# print(phrase.replace("String", "Character")) # to replace any word or letter in a string
+
+
+mystr = "Haris" # string
+        #01234
+print(mystr.index("r"))
