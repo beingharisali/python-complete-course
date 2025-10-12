db8e6629c3f2bbd4a99d0bb37e030d96e21083fa
beingharisali <theharis99@gmail.com>
2025-10-12 18:32:52 +0500

    added custom functions

--- built-in-functions.py
+++ built-in-functions.py
@@ -1,11 +1,11 @@
-# phrase = "Hey This is Just a String"
-# print(phrase.upper()) # to convert string in uppercase
-# print(phrase.lower()) # to convert string in lowercase
-# print(phrase.lower().islower()) # to check if string is in lowercase or uppercase
-# print(len(phrase)) # to check length of a string
-# print(phrase[0]) # to get character of specific index
-# print(phrase.index('Just')) # to get index of a specific character
-# print(phrase.replace("String", "Character")) # to replace any word or letter in a string
+phrase = "Hey This is Just a String"
+print(phrase.upper()) # to convert string in uppercase
+print(phrase.lower()) # to convert string in lowercase
+print(phrase.lower().islower()) # to check if string is in lowercase or uppercase
+print(len(phrase)) # to check length of a string
+print(phrase[0]) # to get character of specific index
+print(phrase.index('Just')) # to get index of a specific character
+print(phrase.replace("String", "Character")) # to replace any word or letter in a string
 
 
 mystr = "Haris" # string
---
+++ functions.py
@@ -0,0 +1,16 @@
+# def greetUser(username, age):
+#     print("Hey " + username + " , How are you? You are " + str(age) + " years old.")
+# greetUser('Marina', 30)
+# greetUser('Haris', 35)
+# greetUser('Juliya', 25)
+
+def sumOfAllValues(num):
+    return num+num+num
+print(sumOfAllValues(5))
+
+def findCube(num):
+    return num*num*num
+print(findCube(5))
+
+# variables -> value -> can be used throught the program
+# parameters -> arguments -> can only be used in a specific function
