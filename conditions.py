dc2a29a251463b6433ef39bb4a2e993a182b8391
beingharisali <theharis99@gmail.com>
2025-10-19 18:37:24 +0500

    conditions

---
+++ conditions.py
@@ -0,0 +1,9 @@
+isMale = True
+isTall = False
+
+if not(isMale) and not(isTall):
+    print('Yes, You are Male as well as tall')
+elif not(isTall) and isMale:
+    print("You are not a short person in height")
+else:
+    print('You are not Male or may be not that much tall')
